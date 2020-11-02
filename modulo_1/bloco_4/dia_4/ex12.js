aee72730a09beb96e9043a99fb98c30ef91da635
rafasysop <rafaelcostamoura@gmail.com>
2020-11-02 11:08:07 -0300

    Enviando Exercicios Bloco 5 dia 1

--- modulo_1/bloco_4/dia_4/ex12.js
+++ modulo_1/bloco_4/dia_4/ex12.js
@@ -1,18 +1,15 @@
-
 function stringComp(word,ending) {
     let ver = false;
     if(ending.length < word.length){
         for(i =1;i<=ending.length;i++){
             if(word[word.length-i] !== ending[ending.length -i]){
-                ver = false;
+                verificar = false;
                 break;
             }else{
-                ver = true;
+                verificar = true;
             }
         }
-
-
-    console.log(ver);
+    console.log(verificar);
     }else{
       console.log('Ending é maior que Word');
     }
