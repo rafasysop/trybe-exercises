8a6d7f66c011934a6e2cb697cbc7974e40b73047
rafasysop <rafaelcostamoura@gmail.com>
2020-11-25 19:06:54 -0300

    Enviando exercicios 1 e 3

---
+++ modulo_1/bloco_7/dia_3/test02.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4]), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
